3d3a3fe860577c8ef62e3c1d86ecdd8250538b6d
KYM-P <k_ym02@naver.com>
2024-03-03 17:15:26 +0900

    2024-03-03

--- AndroidStudio/practice_AndroidStudio/practice_AndroidStudio_java_1/app/src/main/java/com/example/practice_androidstudio_java_1/MainActivity.java
+++ AndroidStudio/practice_AndroidStudio/practice_AndroidStudio_java_1/app/src/main/java/com/example/practice_androidstudio_java_1/MainActivity.java
@@ -7,18 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edt_1;
     Button btn_1;
+    ImageView img_1;
     Button Second_View;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         // EditText
         edt_1 = (EditText)findViewById(R.id.edt_1);
+
         // Button
         btn_1 = (Button)findViewById(R.id.btn_1);
         btn_1.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +32,17 @@ public class MainActivity extends AppCompatActivity {
                 edt_1.setText("Edit!");
             }
         });
+
+        // ImageView
+
+        img_1 = (ImageView)findViewById(R.id.img_1);
+        img_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext() , "Image" , Toast.LENGTH_SHORT).show();
+            }
+        });
+
         // intent(화면전환)
         Second_View = (Button)findViewById(R.id.Second_View);
         Second_View.setOnClickListener(new View.OnClickListener() {
